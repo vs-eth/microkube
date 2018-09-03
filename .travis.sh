274093e357d5964a656ebb4c4b874a61a31316a2
Maximilian Falkenstein <falkensm@ethz.ch>
2018-09-03 16:02:25 +0200

    CI: Rework build yet again
    Try to do package build in one step after running all tests and correct
    build-time dependencies.

--- .travis.sh
+++ .travis.sh
@@ -2,18 +2,22 @@
 
 set -e
 
+
 if [ "${PACKAGE}" = "Yes" ] ; then
-    # Do package build instead
+    # Install packaging dependencies
+    sudo apt update && sudo apt install -y -q rsync golang-go binutils dpkg-dev build-essential fakeroot debhelper dh-golang
+fi
+
+# Do normal test build
+go test -race -coverprofile=cover1 -covermode=atomic -run 'Test[^9]' -v ./...
+sudo killall etcd || /bin/true
+sudo killall hyperkube || /bin/true
+sudo rm -Rf /tmp/microkube* || /bin/true
+go test -coverprofile=cover2 -covermode=atomic -run 'Test9' -v ./...
+cat cover1 cover2 > coverage.txt
+bash <(curl -s https://codecov.io/bash)
+
+if [ "${PACKAGE}" = "Yes" ] ; then
+    # Do additional package build
     dpkg-buildpackage -us -uc -b
 else
-    # Do normal test build
-    go test -race -coverprofile=cover1 -covermode=atomic -run 'Test[^9]' -v ./...
-    sudo killall etcd || /bin/true
-    sudo killall hyperkube || /bin/true
-    sudo rm -Rf /tmp/microkube* || /bin/true
-    go test -coverprofile=cover2 -covermode=atomic -run 'Test9' -v ./...
-
-
-    cat cover1 cover2 > coverage.txt
-    bash <(curl -s https://codecov.io/bash)
-fi
